9cc91e2bae117f00a6cedd01acbe051538627e86
mansour lotfi <mansourlotfi@gmail.com>
2022-04-01 02:21:11 +0430

    minor fix

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -120,7 +120,7 @@ function Footer() {
                     </li>
                     <li>
                       <a href="product_category_sidebar.html">
-                        نوزاد و کودکانه{" "}
+                        نوزاد و کودکانه
                       </a>
                     </li>
                     <li>
@@ -136,7 +136,16 @@ function Footer() {
           </div>
         </div>
 
-        <a href="javascript:void(0);" id="scroll">
+        <div
+          onClick={() => {
+            window.scrollTo({
+              top: 0,
+              left: 0,
+              behavior: "smooth",
+            });
+          }}
+          id="scroll"
+        >
           <svg
             xmlnsXlink="http://www.w3.org/1999/xlink"
             width="15px"
@@ -147,8 +156,8 @@ function Footer() {
               fill="rgb(255, 255, 255)"
               d="M6.638,0.361 C6.638,0.361 6.637,0.361 6.637,0.362 L0.357,6.757 C-0.114,7.237 -0.112,8.012 0.361,8.489 C0.834,8.966 1.599,8.964 2.069,8.485 L6.285,4.191 L6.285,22.770 C6.285,23.446 6.826,23.994 7.493,23.994 C8.160,23.994 8.701,23.446 8.701,22.770 L8.701,4.191 L12.917,8.485 C13.387,8.964 14.152,8.966 14.625,8.489 C15.098,8.012 15.099,7.237 14.629,6.757 L8.349,0.362 C8.349,0.361 8.348,0.361 8.348,0.361 C7.876,-0.119 7.109,-0.117 6.638,0.361 L6.638,0.361 Z"
             />
-          </svg>{" "}
-        </a>
+          </svg>
+        </div>
       </footer>
 
       <div className="e-bfooter-wrap e-bfooter-cloth">
